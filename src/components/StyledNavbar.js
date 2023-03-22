caea34ea9567d5031b591463988129d6283160ec
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-22 21:56:21 +0500

    NavLink added inplace of Link with styles

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+const StyledNavbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/about"
+      >
+        About
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/products"
+      >
+        Products
+      </NavLink>
+    </nav>
+  );
+};
+
+export default StyledNavbar;
